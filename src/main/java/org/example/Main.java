862878be57f74581822b377759ec3607fe0d46e2
Alexander Huynh <alexander_huynh@icloud.com>
2024-02-06 11:55:11 +0100

    feat : setup project structure and code skeleton (#1)
    Co-authored-by: Roxanne <roxanne.chevalley03@gmail.com>
    Co-authored-by: MercuriaL01 <iley.alvarez.funcke@gmail.com>
    Co-authored-by: Rached <rached112004@gmail.com>

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,25 +1,22 @@
+package org.example;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
- 
+
 import java.io.IOException;
- 
+
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
 
-/** 
- Skeleton of a ContinuousIntegrationServer which acts as webhook
- See the Jetty documentation for API documentation of those classes.
-*/
-public class ContinuousIntegrationServer extends AbstractHandler
-{
-    public void handle(String target,
-                       Request baseRequest,
-                       HttpServletRequest request,
-                       HttpServletResponse response) 
+public class Main extends AbstractHandler
+        {
+public void handle(String target,
+        Request baseRequest,
+        HttpServletRequest request,
+        HttpServletResponse response)
         throws IOException, ServletException
-    {
+        {
         response.setContentType("text/html;charset=utf-8");
         response.setStatus(HttpServletResponse.SC_OK);
         baseRequest.setHandled(true);
@@ -32,14 +29,15 @@ public class ContinuousIntegrationServer extends AbstractHandler
         // 2nd compile the code
 
         response.getWriter().println("CI job done");
-    }
- 
-    // used to start the CI server in command line
-    public static void main(String[] args) throws Exception
-    {
+        }
+
+// used to start the CI server in command line
+public static void main(String[] args) throws Exception {
+        System.out.println("Helllo World");
+
         Server server = new Server(8080);
-        server.setHandler(new ContinuousIntegrationServer()); 
+        server.setHandler(new Main());
         server.start();
         server.join();
-    }
-}
+        }
+        }
