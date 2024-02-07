c1639b4a4fac9f08f758c654c1340485c235c2fc
Alexander Huynh <alexander_huynh@icloud.com>
2024-02-07 01:06:22 +0100

    feat : added methods to handle github post requests (#4)
    feat : updated the readme with more instructions (#2)

--- README.md
+++ README.md
@@ -14,3 +14,20 @@ $ java -jar server-ci-group11-1.0-SNAPSHOT-jar-with-dependencies.jar
 ```
 
 If the intention is to run the server through an SSH client, SCP the .jar file to the SSH client. Make sure the SSH client uses JDK17 in $PATH and run the above command.
+
+## ngrok
+
+For GitHub to be able to communicate with the CI server, you should use a tool like ngrok. You will have to download ngrok, register to get an authtoken and configure your ngrok with this token before being able to run ngrok.
+
+## Detaching SSH Clients
+
+For the server to be able to run in the background on the SSH you wanna make use of the screen commands. The following command creates a separate "screen" where you can start the process:
+```bash
+$ screen
+```
+You can then detach from the screen with CTRL+D or equivalent.
+
+To return to your screen to terminate the server for example, you can use the following command:
+```bash
+$ screen -r
+```
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,4 +1,5 @@
 package org.example;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
@@ -9,14 +10,12 @@ import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
 
-public class Main extends AbstractHandler
-        {
-public void handle(String target,
-        Request baseRequest,
-        HttpServletRequest request,
-        HttpServletResponse response)
-        throws IOException, ServletException
-        {
+public class Main extends AbstractHandler {
+    public void handle(String target,
+                       Request baseRequest,
+                       HttpServletRequest request,
+                       HttpServletResponse response)
+            throws IOException, ServletException {
         response.setContentType("text/html;charset=utf-8");
         response.setStatus(HttpServletResponse.SC_OK);
         baseRequest.setHandled(true);
@@ -29,15 +28,15 @@ public void handle(String target,
         // 2nd compile the code
 
         response.getWriter().println("CI job done");
-        }
+    }
 
-// used to start the CI server in command line
-public static void main(String[] args) throws Exception {
+    // used to start the CI server in command line
+    public static void main(String[] args) throws Exception {
         System.out.println("Helllo World");
 
-        Server server = new Server(8080);
+        Server server = new Server(8011);
         server.setHandler(new Main());
         server.start();
         server.join();
-        }
-        }
+    }
+}
