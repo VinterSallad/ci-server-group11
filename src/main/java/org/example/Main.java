6f4fd308d4dd0cd2a0e9551ffa0ab254a1f33536
Alexander Huynh <alexander_huynh@icloud.com>
2024-02-07 01:27:10 +0100

    fix : added missing changes to Main file (#4)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,13 +4,55 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
 
+import org.json.*;
+
 public class Main extends AbstractHandler {
+
+    /**
+     * converts the http request object into a string to be parseable by json library
+     * @param request http request from github
+     * @return http request as a string
+     * @throws IOException
+     */
+    public String httpRequestToString(HttpServletRequest request) throws IOException {
+        StringBuilder stringBuilder = new StringBuilder();
+        BufferedReader reader = request.getReader();
+
+        String line;
+        while((line = reader.readLine()) != null) {
+            stringBuilder.append(line);
+        }
+
+        return stringBuilder.toString();
+    }
+
+    /**
+     * takes a string and converts it into a json object to extract the needed data to clone a github repo
+     * and swap to its relevant branch
+     * @param request post request as a string
+     * @return return the repo url and the relevant push branch as an array in that order
+     */
+    public String[] getGitHubRepo(String request) {
+        JSONObject jsonRequest = new JSONObject(request);
+
+        String repo = jsonRequest.getJSONObject("repository").getString("url");
+        String branch = jsonRequest.getString("ref");
+
+        String[] result = new String[2];
+
+        result[0] = repo;
+        result[1] = branch;
+
+        return result;
+    }
+
     public void handle(String target,
                        Request baseRequest,
                        HttpServletRequest request,
@@ -19,6 +61,24 @@ public class Main extends AbstractHandler {
         response.setContentType("text/html;charset=utf-8");
         response.setStatus(HttpServletResponse.SC_OK);
         baseRequest.setHandled(true);
+
+        //GitHub sends HTTP POST request and we extract the type of event it is and act accordingly
+        //we are interested in "push" https://docs.github.com/en/webhooks/webhook-events-and-payloads#push
+        String githubEvent = request.getHeader("X-GitHub-Event");
+
+        //Example
+        if(githubEvent == "ping") {
+            System.out.println("GitHub successfully communicated with the server!");
+        }
+
+        //TODO
+        if(githubEvent == "push") {
+            String[] test = getGitHubRepo(httpRequestToString(request));
+
+            System.out.println(test[0]);
+            System.out.println(test[1]);
+        }
+
 
         System.out.println(target);
 
