f4a9ed5b4c0124721228a9b00e0e8ff5f24bc64c
rachedkko <rached112004@gmail.com>
2024-02-12 13:46:08 +0100

    feat : add html history function

--- src/main/java/org/example/History.java
+++ src/main/java/org/example/History.java
@@ -31,7 +31,23 @@ public class History {
         }
         return Files.readString(file.toPath());
     }
-    
+    public static int getBuildHistoryHTML() throws IOException {
+        File file = new File("buildHistory.html");
+        if (!file.exists()) {
+            file.createNewFile();
+        }
+        String content = "<!DOCTYPE html>\n<html>\n<head>\n<title>Build History</title>\n</head>\n<body>\n";
+        content += "<h1>Build History</h1>\n";
+        String history = getBuildHistory();
+        String[] builds = history.split("\n");  
+        for (String build : builds) {
+            String[] buildInfo = build.split(" ");
+            content += "<a href=\"builds/" + buildInfo[1] + "\">" + buildInfo[0] + " " + buildInfo[1] + "</a><br>\n";
+        }
+        content += "</body>\n</html>";
+        Files.writeString(file.toPath(), content);
+        return 0;
+    }
 
 
      
