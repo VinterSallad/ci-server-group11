d590bbd150ce7b036e2b25efe43a57d6e110b6a8
Alexander Huynh <alexander_huynh@icloud.com>
2024-02-14 09:39:37 +0100

    testing webhook, should fail

--- src/test/java/org/example/ContinuousIntegrationServerTest.java
+++ src/test/java/org/example/ContinuousIntegrationServerTest.java
@@ -72,7 +72,7 @@ public class ContinuousIntegrationServerTest {
 
     @Test
     public void testGetGitHubRepoRef() {
-        String expected = "refs/heads/main";
+        String expected = "refs/heads/mains";
         String actual = main.getGitHubRepoRef(gitPushPayload);
 
         assertEquals(expected, actual);
